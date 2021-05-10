741a4827884f56849a712fde289ddc7fcd33121e
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2021-05-10 18:18:34 -0300

    adding fx with

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+
+    // tl.resart();#
+rule = CSSruleplugin.getRule("#eland:after")
+   
+gsap.from('.text1', {x: 100, opacity: 0, duration: 1, ease: "linear"});
+gsap.from('.text2', {x: -100, opacity: 0, duration: 1, ease: "linear"});
+// to(rule, {duration: .5, cssRule: {scaleY: 0}}, "");
+// gsap.from(".text2", {x: -100, duration: .5, ease: "linear"});
+// gsap.from(".text1", {opacity: 0, duration: .6, ease: "circ.out"})
+// gsap.from(".text2", {, duration: .6, ease: "circ.out"})
+
+
+
+
