e653f772f76dd85fbebb4771a5e7a777e391b105
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2021-05-16 15:49:17 -0300

    title transition and portal effect

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,16 @@
 
-let tl = gsap.timeline();
-// rule = CSSruleplugin.getRule("#eland:after")
-   
-// gsap.from('.text1', {x: 100, opacity: 0, duration: 1, ease: "linear"});
-// gsap.from('.text2', {x: -100, opacity: 0, duration: 1, ease: "linear"});
+        // let tl = gsap.timeline();
 
-tl.from('.navList', {width: 10, x: 20, opacity: 0, duration: 2, ease: "back"});
+        // tl.from('.navList', {width: 10, x: 20, opacity: 0, duration: 2, ease: "back"});
 
-gsap.from('.text1', {x: 100, opacity: 0, stagger: 5, });
-gsap.from('.text3', {x: 100, opacity: 0, stagger: 5});
+        // gsap.from('.text1', {x: 100, opacity: 0, stagger: 5, });
+        // gsap.from('.text3', {x: 100, opacity: 0, stagger: 5});
 
 
-var titleTl = new TimelineMax({repeat:-1});
-titleTl.fromTo('.text1', 3, {x: 0, opacity: 1}, {x: 100, opacity: 0})
-        .fromTo('.text2', 6, {x: -50, opacity: 1}, {x: 0, opacity: 0})
-        // .fromTo('.text3', 7, {x: 0, opacity: 1}, {x: 100, opacity: 0}, 2)
-        // .fromto('.text2', 7, {x: -50, opacity: 1}, {x: 0, opacity: 0}, 2)
-
-// to(rule, {duration: .5, cssRule: {scaleY: 0}}, "");
-// gsap.from(".text2", {x: -100, duration: .5, ease: "linear"});
-// gsap.from(".text1", {opacity: 0, duration: .6, ease: "circ.out"})
-// gsap.from(".text2", {, duration: .6, ease: "circ.out"})
-
+        // var titleTl = new TimelineMax({repeat:-1});
+        // titleTl.fromTo('.text1', 3, {x: 0, opacity: 1}, {x: 100, opacity: 0})
+        //         .fromTo('.text2', 6, {x: -50, opacity: 1}, {x: 0, opacity: 0})
+ 
 function smoothScroll(target, duration) {
         
         var target = document.querySelector(target);
